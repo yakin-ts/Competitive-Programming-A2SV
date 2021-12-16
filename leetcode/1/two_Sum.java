d3b6d9356fb6ef5773aeced9e3717f20465bb5ef
yakin-ts <59527205+yakin-ts@users.noreply.github.com>
2021-12-16 23:10:38 +0300

    two sum

--- leetcode/1/two_Sum.java
+++ leetcode/1/two_Sum.java
@@ -1,3 +1,16 @@
-public class two_Sum {
-    
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+      int[] result = new int[2];
+        for(int i=0;i<nums.length; i++) {
+            for(int j=i + 1;j<nums.length;j++) {
+                if(target==(nums[i] + nums[j])) {
+                    result[0]=i;
+                    result[1]=j;
+                    
+                }
+            }
+        }
+return result;
 }
+}
+    
