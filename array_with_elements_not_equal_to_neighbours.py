89a11ff38b7b5dac0329af663026e23b226d9936
yakin-ts <59527205+yakin-ts@users.noreply.github.com>
2022-06-02 17:02:26 +0300

    leetcode medium sort and patterns

---
+++ array_with_elements_not_equal_to_neighbours.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        for i in range(1,len(nums)-1):   
+            if((nums[i-1]<nums[i]<nums[i+1]) or (nums[i-1]>nums[i]>nums[i+1])):
+                temp  = nums[i+1]
+                nums[i+1], nums[i] = nums[i], temp
+        return nums
+                
+                
+                
+            
