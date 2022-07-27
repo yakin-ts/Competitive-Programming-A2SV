a24b544057ab76126d7fa83926f4330e88eded54
Yakin Teshome <yakin16tm@gmail.com>
2022-07-27 15:10:09 +0300

    leetcode medium two-pointers greedy

---
+++ container_with_most_water.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l=0
+        r=len(height)-1
+        area=0
+        while l<r:
+            if height[l]<=height[r]:
+                area = max(area, height[l]*(r-l))
+                l+=1
+            else:
+                area = max(area, height[r]*(r-l))
+                r-=1
+        return area
+            
+        
